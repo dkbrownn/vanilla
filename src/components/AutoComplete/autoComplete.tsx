35453a9f1abcc092c09388d26b61a900346274bc
zw.xiong <zw.xiong@qq.com>
2024-11-15 17:15:16 +0800

    fix(autocomplete): 修复文本框清除后一直loading

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -65,7 +65,8 @@ export const AutoComplete = ({
         setSuggestions(results);
       }
     } else {
-      setSuggestions([]);
+      setSuggestions([])
+      setLoading(false)
     }
     triggerSearch.current = false
     setHightLight(-1);
